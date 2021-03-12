74fc426dc96a7a5b6a5e705b3d239fb3b0ba773c
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-12 17:03:50 +0700

    new

--- app/src/main/java/com/aditya/belajarfundamentalandroid/FragmentActivity.kt
+++ app/src/main/java/com/aditya/belajarfundamentalandroid/FragmentActivity.kt
@@ -2,10 +2,30 @@ package com.aditya.belajarfundamentalandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
-class FragmentActivity : AppCompatActivity() {
+class FragmentActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val mFragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name: " + HomeFragment::class.java.simpleName)
+
+            mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+                    .commit()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/aditya/belajarfundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/aditya/belajarfundamentalandroid/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.aditya.belajarfundamentalandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnFragmentActivity: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnFragmentActivity = findViewById(R.id.btn_to_fragment)
+        btnFragmentActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_to_fragment -> {
+                val intent = Intent(this@MainActivity, FragmentActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
