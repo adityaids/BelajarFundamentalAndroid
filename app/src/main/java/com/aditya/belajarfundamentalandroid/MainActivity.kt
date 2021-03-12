440a64cecceac5d92f6f099c636dc508dad2748b
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-12 14:05:44 +0700

    Initial commit

---
+++ app/src/main/java/com/aditya/belajarfundamentalandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aditya.belajarfundamentalandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
